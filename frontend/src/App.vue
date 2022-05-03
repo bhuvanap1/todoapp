1332314289c56477f4b7b06251dd993a8215ac48
Emmanuel Atemba <98485856+bhuvanap1@users.noreply.github.com>
2022-05-03 20:24:59 +0300

    Implement component tree

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div style="text-align: center;">
+<img alt="Vue logo" src="./assets/list.png" height="70" />
+    <HelloWorld msg="Welcome to Task Management App" />
+    </div>
+    
+    <TaskItems/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TaskItems from './components/TaskItems.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,TaskItems
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
